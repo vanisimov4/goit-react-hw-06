a52a3463d269baec455593980d8bc6788c19deec
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-05-08 21:34:27 +0300

    initialReact

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+
+const initialState = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
+
+// Поки що використовуємо редюсер який
+// тільки повертає отриманий стан
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+// Створюємо розширення стора, щоб додати інструменти розробника
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enhancer);
