eb1c3a68598f1f7f93394218eb1f27186b3c1eb1
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-05-12 23:49:16 +0300

    SearchBox

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -1,12 +1,21 @@
 import { useId } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setFilter } from '../../redux/TempActions';
 import css from './SearchBox.module.css';
 
-const SearchBox = ({ onChange }) => {
+const SearchBox = () => {
   const id = useId();
+  const dispatch = useDispatch();
+
+  const handleChangeSearch = event => {
+    dispatch(setFilter(event.target.value));
+  };
+
   return (
     <div className={css.searchBox}>
       <label htmlFor={id}>Find contacts by name</label>
-      <input type="text" id={id} onChange={onChange} />
+      <input type="text" id={id} onChange={handleChangeSearch} />
     </div>
   );
 };
